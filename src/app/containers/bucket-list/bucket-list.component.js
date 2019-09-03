6f9bcd78c73634714b6f0fb1408359aa0cc028f2
eemshi <oreneemshi@gmail.com>
2019-09-02 19:19:32 -0600

    angular add trip functionality

--- src/app/containers/bucket-list/bucket-list.component.js
+++ src/app/containers/bucket-list/bucket-list.component.js
@@ -8,18 +8,25 @@
             controller: BucketListController
         });
 
-    function BucketListController() {
+    function BucketListController($ngRedux) {
 
         const $ctrl = this;
 
-        $ctrl.trips = require('../../data/trips').default;
+        $ctrl.trips = $ngRedux.getState().Trips.trips;
+        $ctrl.trip = {};
 
         $ctrl.$onInit = $onInit;
+        $ctrl.addTrip = addTrip;
 
         function $onInit() {
-
             console.log('trips', $ctrl.trips);
+        }
 
+        function addTrip() {
+            const { Creators } = require('../../../../Redux/Trips/Trips');
+            $ngRedux.dispatch(Creators.addTrip($ctrl.trip));
+            $ctrl.trips = $ngRedux.getState().Trips.trips;
+            $ctrl.trip = {};
         }
 
     }
